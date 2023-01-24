2bcb29a1a7ebeb73e20713cb5d6f496d5f52cf3a
Flavio <flaviourdiales@gmail.com>
2023-01-24 12:42:47 -0600

    reset

--- server.mjs
+++ server.mjs
@@ -1,46 +1,6 @@
 import { createServer } from 'http';
-import {mysql} from 'mysql';
-
-
-
 
 createServer((req, res) => {
   res.write('Hello World!');
-
-
-
-  var con = mysql.createConnection({
-
-    host: "unimundial.edu.mx",
-    user: "wwunim_sci",
-    password : "Cy4?K6,L0kZu",
-    database : "wwunim_sci"
-  });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });  
-
-
-
-
-
-
-
-
-
   res.end();
-
-
-
-
-
-
-
-
-
-
-
-
 }).listen(process.env.PORT);
