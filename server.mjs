1a2ff0428c9e84c716ffb1f99fa924963655cdca
Flavio <flaviourdiales@gmail.com>
2023-01-24 12:27:01 -0600

    a

--- server.mjs
+++ server.mjs
@@ -1,68 +1,6 @@
 import { createServer } from 'http';
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-   host: 'unimundial.edu.mx',
-   user: 'wwunim_sci',
-   password: 'Cy4?K6,L0kZu',
-   database: 'wwunim_sci',
-   port: 3306
-});
-
-
-
-
-
-var app = express();
-
-app.get('/servicios', function(req, res) {
-    var query = connection.query('SELECT * FROM prueba', function(err, rows, fields) {
-        if (err) throw err;
-        res.json(rows);
-    }
-    );
-});
-
-app.post('/servicios/:tabla', function(req, res) {
-    var query = connection.query('INSERT INTO ? SET ?', [req.params.tabla, req.body], function(err, rows, fields) {
-        if (err) throw err;
-        res.json(result);
-    });
-});
-
-
-app.put('/servicios/:id', function(req, res) {
-    var query = connection.query('UPDATE prueba SET ? WHERE id = ?', [req.body, req.params.id], function(err, result) {
-        if (err) throw err;
-        res.json(result);
-    }
-    );
-
-});
-
-
-app.listen(3000, function() {
-  console.log('Aplicación ejemplo, escuchando el puerto 3000!');
-
-  connection.connect(function(error){
-    if(error){
-       throw error;
-    }else{
-       console.log('Conexion correcta.');
-    }
- });
-
-});
-
-app.listen((req, res) => {
+createServer((req, res) => {
   res.write('Hello World!');
   res.end();
 }).listen(process.env.PORT);
-
-
-
-
-
-
-
-
