ec9af11dd016c0c98eac7671ef4001d5394c0fe9
Flavio <flaviourdiales@gmail.com>
2023-01-24 12:35:26 -0600

    prueba4

--- server.mjs
+++ server.mjs
@@ -1,4 +1,20 @@
 import { createServer } from 'http';
+var mysql = require('mysql');
+var con = mysql.createConnection({
+
+  host: "unimundial.edu.mx",
+  user: "wwunim_sci",
+  password : "Cy4?K6,L0kZu",
+  database : "wwunim_sci"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+
 
 createServer((req, res) => {
   res.write('Hello World!');
