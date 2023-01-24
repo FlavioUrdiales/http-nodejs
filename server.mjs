1f46ea449a051bcf87ce661b6c3a300750318a2e
Flavio <flaviourdiales@gmail.com>
2023-01-24 12:18:51 -0600

    Prueba2

--- server.mjs
+++ server.mjs
@@ -6,20 +6,3 @@ createServer((req, res) => {
 }).listen(process.env.PORT);
 
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-   host: 'unimundial.edu.mx',
-   user: 'wwunim_sci',
-   password: 'Cy4?K6,L0kZu',
-   database: 'wwunim_sci',
-   port: 3306
-});
-
-connection.connect(function(error){
-   if(error){
-      throw error;
-   }else{
-      console.log('Conexion correcta.');
-   }
-});
-connection.end();
